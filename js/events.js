ed7c4af6b872be74b4749ed49753653fc41eddeb
Kevin Minutti <minutti70@gmail.com>
2020-03-28 04:27:15 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,14 +8,27 @@ function getIt() {
 }
 
 function frameIt() {
-
+ $('img').on('load', function() {
+   $('img').addClass('tasty');
+ });
 }
 
 function pressIt() {
-
+ $(document).on('keydown', function(key) {
+   if (key.which === 71){
+     alert('G was pressed')}
+ });
 }
 
 function submitIt() {
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 
 }
 
